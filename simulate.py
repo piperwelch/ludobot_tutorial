e82d80029b8a5e41f556e88c6747803a1e97e2a0
piperwelch <welchp@carleton.edu>
2022-06-17 16:03:46 -0500

    Added GUI

--- simulate.py
+++ simulate.py
@@ -1,4 +1,9 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+for i in range(1000):
+    time.sleep(0.01667)
+    p.stepSimulation()
+    print("Loop #" + str(i) + "\n")
 p.disconnect()
